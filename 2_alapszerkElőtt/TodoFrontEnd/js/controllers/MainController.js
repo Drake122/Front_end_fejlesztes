71fc4d518db42eaa981fdbf5f906b9e1d9ee3f5d
DrakeCompany <kovacs.sandor.dolphio@gmail.com>
2016-10-17 20:04:23 +0200

    alap

---
+++ 2_alapszerkElőtt/TodoFrontEnd/js/controllers/MainController.js
@@ -0,0 +1,26 @@
+app.controller('MainCtrl', ['$scope', function ($scope) {
+
+    $scope.myData = [
+        {
+            "firstName": "Cox",
+            "lastName": "Carney",
+            "company": "Enormo",
+            "employed": true,
+            "vaslami":"valami"
+        },
+        {
+            "firstName": "Lorraine",
+            "lastName": "Wise",
+            "company": "Comveyer",
+            "employed": false,
+            "vaslami":"valami"
+        },
+        {
+            "firstName": "Nancy",
+            "lastName": "Waters",
+            "company": "Fuelton",
+            "employed": false,
+            "vaslami":"valami"
+        }
+    ];
+}]);
