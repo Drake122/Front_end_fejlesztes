d9baaf1217fa3e84e4fdcb0775816a00f9cdb03a
DrakeCompany <kovacs.sandor.dolphio@gmail.com>
2016-10-17 20:21:39 +0200

    Jó_checkbox

--- Web_storm_project/TodoFrontEnd/js/controllers/MainController.js
+++ Web_storm_project/TodoFrontEnd/js/controllers/MainController.js
@@ -5,14 +5,14 @@ app.controller('MainCtrl', ['$scope', '$http', '$log', function ($scope, $http, 
 
 
     $scope.gridOptions.columnDefs = [
-        { name: 'idtask', enableCellEdit: false, width: '5%' },
-        { name: 'label', displayName: 'Label (editable)', width: '10%' },
+        { name: 'idtask', enableCellEdit: false, width: '2%' },
+        { name: 'label', displayName: 'Label (editable)', width: '5%' },
         { name: 'description',displayName:'Description (editable)', width: '30%' },
         { name: 'status', displayName: 'Status', type:'number',width:'10%'},
-        { name: 'startTime', displayName: 'startTime' , type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '20%' },
-        { name: 'finishTime', displayName: 'finishTime' , type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '20%' },
-        { name: 'priority', displayName: 'Priority', type:'number',width:'10%'},
-        { name: 'responsible', displayName: 'responsible', type:'number',width:'10%'},
+        { name: 'startTime', displayName: 'startTime' , type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '10%' },
+        { name: 'finishTime', displayName: 'finishTime' , type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '10%' },
+        { name: 'priority', displayName: 'Priority', type:'number',width:'6%'},
+        { name: 'responsible', displayName: 'responsible', type:'number',width:'7%'},
         { name: 'userCollection', displayName: 'Users', type: 'object', enableCellEdit: false, width: '20%' }
     ];
 
@@ -25,6 +25,7 @@ app.controller('MainCtrl', ['$scope', '$http', '$log', function ($scope, $http, 
         gridApi.edit.on.afterCellEdit($scope,function(rowEntity, colDef, newValue, oldValue){
             $scope.msg.lastCellEdited = 'edited row id:' + rowEntity.idtask + ' Column:' + colDef.name + ' newValue:' + newValue + ' oldValue:' + oldValue ;
             console.log(rowEntity);
+           // console.log($scope.statuses);
             $scope.$apply();
         });
     };
@@ -43,6 +44,31 @@ app.controller('MainCtrl', ['$scope', '$http', '$log', function ($scope, $http, 
             values.push(currentSelection[i].row.entity[currentSelection[i].col.name])
         }
         $scope.printSelection = values.toString();
+    };
+    $scope.user = {
+        status: [2, 3]
+    };
+
+    $http.get('http://localhost:8080/user/allUser')
+        .success(function(data) {
+            console.log(data);
+
+        });
+
+    $scope.statuses = [
+        {value: 1, text: 'status1'},
+        {value: 2, text: 'status2'},
+        {value: 3, text: 'status3'}
+    ];
+
+    $scope.showStatus = function() {
+        var selected = [];
+        angular.forEach($scope.statuses, function(s) {
+            if ($scope.user.status.indexOf(s.value) >= 0) {
+                selected.push(s.text);
+            }
+        });
+        return selected.length ? selected.join(', ') : 'Not set';
     };
 
 
