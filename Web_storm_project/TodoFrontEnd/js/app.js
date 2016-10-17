d3dddbd08f220d326700dfd3f5452fe7da0912f7
DrakeCompany <kovacs.sandor.dolphio@gmail.com>
2016-10-17 20:33:05 +0200

    +bootstrap

--- Web_storm_project/TodoFrontEnd/js/app.js
+++ Web_storm_project/TodoFrontEnd/js/app.js
@@ -1,6 +1,15 @@
 // var app = angular.module('app', ['ngTouch', 'ui.grid']);
 
-var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.edit','ui.grid.cellNav', 'ui.grid.pinning','xeditable','checklist-model', 'ngAnimate']);
+var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.edit', 'ui.grid.cellNav', 'ui.grid.pinning', 'xeditable', 'checklist-model', 'ngAnimate']);
+
+app.config(function ($httpProvider) {
+    //Enable cors
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.withCredentials = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
 
 
-
+app.run(function(editableOptions) {
+    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
--- Web_storm_project/TodoFrontEnd/js/controllers/MainController.js
+++ Web_storm_project/TodoFrontEnd/js/controllers/MainController.js
@@ -1,4 +1,4 @@
-app.controller('MainCtrl', ['$scope', '$http', '$log','uiGridConstants', function ($scope, $http,uiGridConstants, $log) {
+app.controller('MainCtrl', ['$scope', '$http', '$log', 'uiGridConstants', function ($scope, $http, uiGridConstants, $log) {
     $scope.gridOptions = {
         enableFiltering: true,
         modifierKeysToMultiSelectCells: true,
@@ -6,17 +6,19 @@ app.controller('MainCtrl', ['$scope', '$http', '$log','uiGridConstants', functio
         enableSorting: true,
         enableCellEditOnFocus: true,
         columnDefs: [
-            { name: 'field1', enableSorting: false, enableCellEdit: false },
-            { name: 'field2' },
-            { name: 'field3', visible: false }]
+            {name: 'field1', enableSorting: false, enableCellEdit: false},
+            {name: 'field2'},
+            {name: 'field3', visible: false}]
     };
+
+
 //DELETE + RESET  ///////////////
 
 
-    $scope.addData = function() {
+    $scope.addData = function () {
         var n = $scope.gridOptions.data.length + 1;
         $scope.gridOptions.data.push({
-            "idtask":  n,
+            "idtask": n,
             "label": "label",
             "description": "Description",
             "status": "Status",
@@ -29,17 +31,16 @@ app.controller('MainCtrl', ['$scope', '$http', '$log','uiGridConstants', functio
     };
 
 
-
     var columnDefs1 = [
-        { name: 'idtask' },
-        { name: 'label' },
-        { name: 'description' },
-        { name: 'status' },
-        { name: 'startTime' },
-        { name: 'finishTime' },
-        { name: 'priority' },
-        { name: 'responsible' },
-        { name: 'userCollection' }
+        {name: 'idtask'},
+        {name: 'label'},
+        {name: 'description'},
+        {name: 'status'},
+        {name: 'startTime'},
+        {name: 'finishTime'},
+        {name: 'priority'},
+        {name: 'responsible'},
+        {name: 'userCollection'}
     ];
 
     var data1 = $scope.gridOptions.data;
@@ -50,41 +51,49 @@ app.controller('MainCtrl', ['$scope', '$http', '$log','uiGridConstants', functio
     };
 
 
-
 //DELETE + RESET////////////
 
     $scope.gridOptions.columnDefs = [
-        { name: 'idtask',displayName: 'Id', enableCellEdit: false, width: '4%' },
-        { name: 'label', displayName: 'Label (editable)',headerCellClass: $scope.highlightFilteredHeader, width: '5%' },
-        { name: 'description',displayName:'Description (editable)', width: '30%' },
-        { name: 'status', displayName: 'Status', type:'number',width:'10%'},
-        { name: 'startTime', displayName: 'startTime' , type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '8%' },
-        { name: 'finishTime', displayName: 'finishTime' , type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '10%' },
-        { name: 'priority', displayName: 'Priority', type:'number',width:'6%'},
-        { name: 'responsible', displayName: 'responsible', type:'number',width:'7%'},
-        { name: 'userCollection', displayName: 'Users', type: 'object', /*enableCellEdit: false,*/ width: '20%' }
+        {name: 'idtask', displayName: 'Id', enableCellEdit: false, width: '4%'},
+        {name: 'label', displayName: 'Label (editable)', headerCellClass: $scope.highlightFilteredHeader, width: '5%'},
+        {name: 'description', displayName: 'Description (editable)', width: '30%'},
+        {name: 'status', displayName: 'Status', type: 'number', width: '10%'},
+        {name: 'startTime', displayName: 'startTime', type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '8%'},
+        {name: 'finishTime', displayName: 'finishTime', type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '10%'},
+        {name: 'priority', displayName: 'Priority', type: 'number', width: '6%'},
+        {name: 'responsible', displayName: 'responsible', type: 'number', width: '7%'},
+        {name: 'userCollection', displayName: 'Users', type: 'object', /*enableCellEdit: false,*/ width: '20%'}
     ];
 
     $scope.msg = {};
-    $scope.editedUsers=[];
-    $scope.gridOptions.onRegisterApi = function(gridApi){
+    $scope.editedUsers = [];
+    $scope.gridOptions.onRegisterApi = function (gridApi) {
         $scope.gridApi = gridApi;
-        gridApi.edit.on.afterCellEdit($scope,function(rowEntity, colDef, newValue, oldValue){
-            $scope.msg.lastCellEdited = 'edited row id:' + rowEntity.idtask + ' Column:' + colDef.name + ' newValue:' + newValue + ' oldValue:' + oldValue ;
-         /*  if($scope.editedUsers.length>0){
-               rowEntity.userCollection=$scope.editedUsers;
-               $scope.editedUsers=[];
-           };*/
+        gridApi.edit.on.afterCellEdit($scope, function (rowEntity, colDef, newValue, oldValue) {
+            $scope.msg.lastCellEdited = 'edited row id:' + rowEntity.idtask + ' Column:' + colDef.name + ' newValue:' + newValue + ' oldValue:' + oldValue;
+            /*  if($scope.editedUsers.length>0){
+             rowEntity.userCollection=$scope.editedUsers;
+             $scope.editedUsers=[];
+             };*/
 
             console.log(rowEntity);
             $scope.$apply();
-           // $http.put('http://localhost:8080/updateTask/{id}',rowEntity.idtask,rowEntity);
+            $http.put({
+                url: 'http://localhost:8080/task//updateTaskById/{id}',
+                data: {
+                    id: rowEntity.idtask,
+                    data: rowEntity
+                },
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                }
+            });
 
         });
     };
 
     $http.get('http://localhost:8080/task/allTask')
-        .success(function(data) {
+        .success(function (data) {
             $scope.gridOptions.data = data;
         });
 
@@ -93,13 +102,13 @@ app.controller('MainCtrl', ['$scope', '$http', '$log','uiGridConstants', functio
 
     $scope.user = {status: []};
 
-    $scope.getCurrentSelection = function() {
+    $scope.getCurrentSelection = function () {
         $scope.user = {status: []};
         var values = [];
         var currentSelection = $scope.gridApi.cellNav.getCurrentSelection();
-            values.push(currentSelection[0].row.entity[currentSelection[0].col.name]);
+        values.push(currentSelection[0].row.entity[currentSelection[0].col.name]);
         $scope.printSelection = values.toString();
-        angular.forEach(currentSelection[0].row.entity.userCollection, function (val,key) {
+        angular.forEach(currentSelection[0].row.entity.userCollection, function (val, key) {
             $scope.user.status.push(val);
         })
     };
@@ -108,38 +117,38 @@ app.controller('MainCtrl', ['$scope', '$http', '$log','uiGridConstants', functio
     $scope.statuses = [];
 
     $http.get('http://localhost:8080/user/allUser')
-        .success(function(data) {
-          //  console.log( $scope.statuses);
-        $scope.allUsers=data;
+        .success(function (data) {
+            //  console.log( $scope.statuses);
+            $scope.allUsers = data;
             angular.forEach($scope.allUsers, function (u) {
-                var value =u.iduser;     var text = u.name;
-                var user =  {value:value, text:text};
-               $scope.statuses.push( user);
+                var value = u.iduser;
+                var text = u.name;
+                var user = {value: value, text: text};
+                $scope.statuses.push(user);
             })
         });
 
-    $scope.showStatus = function() {
+    $scope.showStatus = function () {
         var selected = [];
-        $scope.editedUsers=[];
-        angular.forEach($scope.statuses, function(s) {
+        $scope.editedUsers = [];
+        angular.forEach($scope.statuses, function (s) {
             if ($scope.user.status.indexOf(s.value) >= 0) {
                 selected.push(s.text);
             }
         });
-        $scope.editedUsers=selected;
+        $scope.editedUsers = selected;
         return selected.length ? selected.join(', ') : 'Not set';
     };
 
 
     // Keresés///////////////////
-    $scope.highlightFilteredHeader = function( row, rowRenderIndex, col, colRenderIndex ) {
-        if( col.filters[0].term ){
+    $scope.highlightFilteredHeader = function (row, rowRenderIndex, col, colRenderIndex) {
+        if (col.filters[0].term) {
             return 'header-filtered';
         } else {
             return '';
         }
     };
-
 
 
 }]);
